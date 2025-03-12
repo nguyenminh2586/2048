fc7710c29c98f4e1fade50ff46b1fd8b07e4fac2
Nguyen Ngoc Minh <minh2k62508@gmail.com>
2025-03-12 07:29:38 +0700

    update

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #define SDL_MAIN_HANDLED
 #include <SDL.h>
 #include <SDL_ttf.h>
+#include <SDL_mixer.h>
 #include <iostream>
 #include <ctime>
 #include <cstdlib>
@@ -8,20 +9,24 @@
 
 using namespace std;
 
-const int SCREEN_WIDTH = 400;
-const int SCREEN_HEIGHT = 400;
+const int SCREEN_WIDTH = 500;
+const int SCREEN_HEIGHT = 600;
 const int GRID_SIZE = 4;
 const int TILE_SIZE = 100;
+const int BOARD_X = 50;
+const int BOARD_Y = 150;
 const int winPoint = 2048;
 
 SDL_Window* window = nullptr;
 SDL_Renderer* renderer = nullptr;
 TTF_Font* font = nullptr;
 int board[GRID_SIZE][GRID_SIZE];
+int score = 0;
+int bestScore = 0;
+bool showMenu = true;
 
 int dirLine[] = {1, 0, -1, 0};
 int dirColumn[] = {0, 1, 0, -1};
-
 map<int, SDL_Color> tileColors = {
     {0, {205, 193, 180}}, {2, {238, 228, 218}}, {4, {237, 224, 200}},
     {8, {242, 177, 121}}, {16, {245, 149, 99}}, {32, {246, 124, 95}},
@@ -29,20 +34,32 @@ map<int, SDL_Color> tileColors = {
     {512, {237, 200, 80}}, {1024, {237, 197, 63}}, {2048, {237, 194, 46}}
 };
 
-void initSDL() {
-    SDL_Init(SDL_INIT_VIDEO);
-    TTF_Init();
-    window = SDL_CreateWindow("2048 Game", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
-    renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
-    font = TTF_OpenFont("arial.ttf", 24);
+void renderText(const string &text, int x, int y) {
+    SDL_Surface* textSurface = TTF_RenderText_Solid(font, text.c_str(), {0, 0, 0});
+    SDL_Texture* textTexture = SDL_CreateTextureFromSurface(renderer, textSurface);
+    SDL_Rect textRect = {x, y, textSurface->w, textSurface->h};
+    SDL_RenderCopy(renderer, textTexture, NULL, &textRect);
+    SDL_FreeSurface(textSurface);
+    SDL_DestroyTexture(textTexture);
 }
 
-void closeSDL() {
-    TTF_CloseFont(font);
-    SDL_DestroyRenderer(renderer);
-    SDL_DestroyWindow(window);
-    TTF_Quit();
-    SDL_Quit();
+void renderBoard() {
+    for (int i = 0; i < GRID_SIZE; i++) {
+        for (int j = 0; j < GRID_SIZE; j++) {
+            SDL_Rect tile = {BOARD_X + j * TILE_SIZE, BOARD_Y + i * TILE_SIZE, TILE_SIZE - 5, TILE_SIZE - 5};
+            SDL_Color color = tileColors[board[i][j]];
+            SDL_SetRenderDrawColor(renderer, color.r, color.g, color.b, 255);
+            SDL_RenderFillRect(renderer, &tile);
+            if (board[i][j] != 0) {
+                renderText(to_string(board[i][j]), BOARD_X + j * TILE_SIZE + TILE_SIZE / 3, BOARD_Y + i * TILE_SIZE + TILE_SIZE / 3);
+            }
+        }
+    }
+}
+
+void renderScore() {
+    renderText("Score: " + to_string(score), 50, 50);
+    renderText("Best: " + to_string(bestScore), 250, 50);
 }
 
 pair<int, int> generateUnoccupiedPosition() {
@@ -95,6 +112,8 @@ void applyMove(int direction) {
                         canAddPiece = 1; // Đánh dấu có di chuyển hợp lệ
                     } else if (board[nextI][nextJ] == board[i][j]) {
                         board[nextI][nextJ] += board[i][j];
+                        score += board[nextI][nextJ];
+                        if (score > bestScore) bestScore = score;
                         board[i][j] = 0;
                         movePossible = 1;
                         canAddPiece = 1; // Đánh dấu có hợp nhất
@@ -109,53 +128,9 @@ void applyMove(int direction) {
 void render() {
     SDL_SetRenderDrawColor(renderer, 187, 173, 160, 255);
     SDL_RenderClear(renderer);
-
-    for (int i = 0; i < GRID_SIZE; i++) {
-        for (int j = 0; j < GRID_SIZE; j++) {
-            SDL_Rect tile = {j * TILE_SIZE, i * TILE_SIZE, TILE_SIZE - 5, TILE_SIZE - 5};
-            SDL_Color color = tileColors[board[i][j]];
-            SDL_SetRenderDrawColor(renderer, color.r, color.g, color.b, 255);
-            SDL_RenderFillRect(renderer, &tile);
-
-            if (board[i][j] != 0) {
-                SDL_Surface* textSurface = TTF_RenderText_Solid(font, to_string(board[i][j]).c_str(), {0, 0, 0});
-                SDL_Texture* textTexture = SDL_CreateTextureFromSurface(renderer, textSurface);
-                SDL_Rect textRect = {j * TILE_SIZE + TILE_SIZE / 4, i * TILE_SIZE + TILE_SIZE / 4, textSurface->w, textSurface->h};
-                SDL_RenderCopy(renderer, textTexture, nullptr, &textRect);
-                SDL_FreeSurface(textSurface);
-                SDL_DestroyTexture(textTexture);
-            }
-        }
-    }
+    renderScore();
+    renderBoard();
     SDL_RenderPresent(renderer);
-}
-
-bool isGameOver() {
-    for (int i = 0; i < GRID_SIZE; i++)
-        for (int j = 0; j < GRID_SIZE; j++)
-            if (board[i][j] == 0)
-                return false;
-    for (int i = 0; i < GRID_SIZE; i++)
-        for(int j = 0; j < GRID_SIZE; j++)
-            for(int dir = 0; dir < GRID_SIZE; dir++)
-            {
-                int nextI = i + dirLine[dir];
-                int nextJ = j + dirColumn[dir];
-                if(nextI > 0 && nextJ > 0 && nextI < GRID_SIZE && nextJ < GRID_SIZE)
-                {
-                    if(board[i][j]==board[nextI][nextJ])
-                        return false;
-                }
-            }
-    return true;
-}
-
-bool hasWon() {
-    for (int i = 0; i < GRID_SIZE; i++)
-        for (int j = 0; j < GRID_SIZE; j++)
-            if (board[i][j] == winPoint)
-                return true;
-    return false;
 }
 
 void gameLoop() {
@@ -166,35 +141,39 @@ void gameLoop() {
             if (e.type == SDL_QUIT) running = false;
             if (e.type == SDL_KEYDOWN) {
                 switch (e.key.keysym.sym) {
-                    case SDLK_w: applyMove(2); break;
                     case SDLK_UP: applyMove(2); break;
-                    case SDLK_s: applyMove(0); break;
                     case SDLK_DOWN: applyMove(0); break;
-                    case SDLK_a: applyMove(3); break;
                     case SDLK_LEFT: applyMove(3); break;
-                    case SDLK_d: applyMove(1); break;
                     case SDLK_RIGHT: applyMove(1); break;
-                    case SDLK_n: newGame(); break;
-
+                    case SDLK_n: memset(board, 0, sizeof(board)); addPiece(); score = 0; break;
                 }
             }
         }
         render();
-        if (isGameOver()) {
-            cout << "Game Over!" << endl;
-            running = false;
-        }
-        if (hasWon()) {
-            cout << "You Win!" << endl;
-            running = false;
-        }
     }
+}
+
+void initSDL() {
+    SDL_Init(SDL_INIT_VIDEO);
+    TTF_Init();
+    window = SDL_CreateWindow("2048 Game", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+    renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+    font = TTF_OpenFont("arial.ttf", 24);
+}
+
+void closeSDL() {
+    TTF_CloseFont(font);
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    TTF_Quit();
+    SDL_Quit();
 }
 
 int main() {
     srand(time(0));
     initSDL();
-    newGame();
+    memset(board, 0, sizeof(board));
+    addPiece();
     gameLoop();
     closeSDL();
     return 0;
